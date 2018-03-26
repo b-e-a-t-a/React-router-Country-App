58eaa27e3d1128f9ede005a86050eec53e4391c9
Beata Karolczak <karolczak.beata@gmail.com>
2018-03-27 00:34:18 +0200

    Add delete functionality

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import CountryFlagList from '../presentational/flag-list.component';
-import {getCountries, searchCountries} from '../actions/actions-countries';
+import {getCountries, searchCountries, deleteCountry} from '../actions/actions-countries';
 
 class CountryFlagContainer extends Component {
 	constructor(props) {
@@ -14,13 +14,16 @@ class CountryFlagContainer extends Component {
 	search(event) {
 		this.props.dispatch(searchCountries(event.target.value));
 	}
+	deleteCountry(id) {
+		this.props.dispatch(deleteCountry(id));
+	}
 	render() {
 		return (
 			<div>
 				<div className="search text-center">
 					<input type="text" onChange={this.search.bind(this)}/>
 				</div>
-				<CountryFlagList countries={this.props.visibleCountries} />
+				<CountryFlagList countries={this.props.visibleCountries} deleteCountry={this.deleteCountry.bind(this)}/>
 			</div>
 		)
 	}
